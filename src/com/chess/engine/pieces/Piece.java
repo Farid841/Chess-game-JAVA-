9cde4fecdec2b19c47c2efeed2afe414f7ca31a6
Farid <farid.ousmane01@gmail.com>
2022-12-21 20:37:38 +0100

    Introducing the definition of piece class

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,6 +1,20 @@
 package com.chess.engine.pieces;
 
-public class Piece {
-    /*** **/
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
 
+import java.util.List;
+
+public abstract class Piece {
+    /** **/
+    protected final int piece_position;
+    protected final Alliance piece_alliance;
+
+    Piece(final int piece_position, final Alliance piece_alliance){
+        this.piece_position = piece_position;
+        this.piece_alliance = piece_alliance;
+    }
+
+    public abstract List<Move> calculateLegalMoves(final Board board);
 }
